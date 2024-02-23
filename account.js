e3cd25f1667336e2469b31984744241a8a153d23
Mimicoctopus1 <87786838+Mimicoctopus1@users.noreply.github.com>
2024-02-23 12:49:54 -0800

    Update account.js

--- account.js
+++ account.js
@@ -1,7 +1,7 @@
 var account = {
   "requestAccess": function(granted, denied) {
     let requestAccessPopup = window.open("", "UNIMONO_Account_Verification", "");
-    requestAccessPopup.document.body.innerHTML = "<!DOCTYPE html><html><h1>The webpage at " + window.document.location.href/*Give the popup this webpage's URL*/ + "</h1></html>"
+    requestAccessPopup.document.body.innerHTML = "<!DOCTYPE html><html><h1>The webpage at " + window.document.location.href/*Give the popup this webpage's URL*/ + " would like access to your account. Notice that, if you continue, full access to your account may be granted. This involves privileges to know up to any amount of data from your account.</h1></html>"
   }
 };
 export default account;
