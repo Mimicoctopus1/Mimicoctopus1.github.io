8fd0ede07b80b8bc6f1a1deb9d7c99b309462994
Mimicoctopus1 <87786838+Mimicoctopus1@users.noreply.github.com>
2024-10-04 12:54:15 -0700

    Delete public directory

--- public/service-worker.js
+++
@@ -1,8 +0,0 @@
-/*
-This file does the stuff like:
-Send notifications,
-cache everything on your phone for offline use,
-and more.
-(All stuff that only needs to be done when this app is downloaded.)
-*/
-/*Welp, nothing yet!*/
